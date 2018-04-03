cb4382acd9a0a72840fde4768d6c5fadee6ec32f
R.C. Long <deac.long@wsu.edu>
2018-04-03 07:44:45 -0700

    Add btn style and initial AJAX functions

--- src/controllers/stockController.client.js
+++ src/controllers/stockController.client.js
@@ -1,1 +1,21 @@
-console.log("Script retrieved")
+var active = { id: "" };
+
+$(document).ready(function () {
+  var initialId = "y-b";
+  $(`#${initialId}`).addClass("active");
+  active.id = initialId;
+
+  // $.get("", function(data) {
+  //   console.log(data);
+  // })
+});
+
+function handleAdd() {
+
+}
+
+function handleTFClick(id) {
+  $(`#${active.id}`).removeClass("active");
+  $(`#${id}`).addClass("active");
+  active.id = id;
+}
