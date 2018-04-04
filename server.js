d2f40f6016538bdec80cfb193c44a40e076b18d8
R.C. Long <deac.long@wsu.edu>
2018-04-03 18:03:53 -0700

    Remove REST routes and add socket namespaces

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 
 const express = require('express')
 const app = express()
+const http = require('http').Server(app)
 const mongoose = require('mongoose')
+const io = require('socket.io')(http)
 const PORT = 5000
 
 require('dotenv').load()
@@ -16,8 +18,10 @@ app.use('/common', express.static(process.cwd() + '/src/common'))
 
 app.set('port', process.env.PORT || PORT)
 
-var server = app.listen(app.get('port'), () => {
+var server = http.listen(app.get('port'), () => {
   console.log(`Server is running on port ${server.address().port}...`)
 })
 
 var routes = require('./src/routes/routes.js')(app)
+
+const namespaces = require('./src/namespaces/namespaces.js')(io)
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,12 +8,6 @@ var routes = function (app) {
     .get(function (req, res) {
       res.sendFile(path + '/public/index.html')
     })
-
-  app.route('/api/stocks')
-    .get(Stock.getStocks)
-
-  app.route('/api/stock/:symbol')
-    .post(Stock.addStock)
 }
 
 module.exports = routes
